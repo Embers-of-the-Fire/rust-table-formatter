5f8d5db94a2a92ab518fdc3db8226b423824fbb0
Embers-of-the-Fire <stellarishs@163.com>
2023-06-07 19:05:47 +0800

    edit readme

--- tests/test_table.rs
+++ tests/test_table.rs
@@ -1,39 +1,39 @@
 use itertools::Itertools;
-use table_formatter::table::*;
+use table_formatter::{cell, table, table::*};
 
 #[test]
 fn test_table_formatter() {
-    let table = Table::new(
-        (0..=4)
-            .map(|i1| {
-                if i1 % 2 == 0 {
-                    let mut v = vec![Cell::default().with_content(Content::Splitter)];
-                    v.append(
-                        &mut Cell::default()
-                            .with_content(Content::new("123123123123123123123123123"))
-                            .with_span(1),
-                    );
-                    v.push(Cell::default().with_content(Content::Splitter));
-                    v
-                } else {
-                    (0..4)
-                        .map(|i2| {
-                            Cell::default().with_content(Content::new(format!("{}-{}", i1, i2)))
-                        })
-                        .collect_vec()
-                }
+    let table_header = vec![
+        cell!("Cell Row").with_width(Some(20)),
+        cell!("Left", align = Align::Left).with_width(Some(10)),
+        cell!("Center", align = Align::Center).with_width(Some(10)),
+        cell!("Right", align = Align::Right).with_width(Some(10)),
+    ];
+    let table_cells = {
+        let mut v = (0..=3_u8)
+            .map(|_| {
+                vec![
+                    cell!("Cell Row"),
+                    cell!("Left", align = Align::Left),
+                    cell!("Center", align = Align::Center),
+                    cell!("Right", align = Align::Right),
+                ]
             })
-            .collect_vec(),
-    )
-    .with_border(Border::ALL);
+            .collect_vec();
+        v.push(cell!("Cross Cell!", align = Align::Center).with_span(3));
+        v
+    };
+    let table = table! {
+        table_header
+        ---
+        table_cells
+        with Border::ALL
+    };
 
-    let mut r = Vec::new();
-    let result = table.render(&mut r);
-    if let Err(e) = result {
-        println!("{}", e)
-    } else {
-        println!("{}", String::from_utf8(r).unwrap())
-    }
+    let mut buffer = vec![];
+    table.render(&mut buffer).unwrap();
+
+    println!("{}", String::from_utf8(buffer).unwrap());
 }
 
 #[test]
